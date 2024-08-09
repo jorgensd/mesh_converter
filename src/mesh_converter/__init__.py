8d7e168c8b99575c92e6390c9c2c02dd07ebb93e
jorgensd <dokken92@gmail.com>
2024-08-09 11:31:44 +0200

    Expose xdmf module to do custom stuff

--- src/mesh_converter/__init__.py
+++ src/mesh_converter/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["read_exodus2_data", "write_mesh", "Mesh", "CellType"]
+__all__ = ["read_exodus2_data", "write_mesh", "Mesh", "CellType", "xdmf"]
 
 from mesh_converter.exodus2_converter import read_exodus2_data
 from mesh_converter.mesh import CellType, Mesh
+import mesh_converter.xdmf as xdmf
